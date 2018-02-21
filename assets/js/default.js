8375044aa28829f8c4f1b77a916f4ad5294322bd
haitai <github@huhaitai.com>
2018-02-21 12:41:51 +0800

    first commit

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,25 +1,32 @@
 var colorStatus = false,
     infoStatus = false;
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+var hou = today.getHours(); //Returns the hour of the day (0-23).
+var min = today.getMinutes(); //Returns the minute (0-59)
+var sec = today.getSeconds(); //Returns the second (0-59).
     if(dd<10) {
         dd = '0'+dd
     } 
-
     if(mm<10) {
         mm = '0'+mm
     } 
-
-    today = yyyy + '-' + mm + '-' + dd;
+    if(min<10) {
+        min = '0'+min
+    } 
+    if(sec<10) {
+        sec = '0'+sec
+    } 
+    todaydate = yyyy + '-' + mm + '-' + dd;
+    nowtime = yyyy + '-' + mm + '-' + dd + 'T' + hou + ':' + min + ':' + sec + '+08:00';
     np='---\n\
 title:\n\
 author: admin\n\
 type: post\n\
 date:'
-+ ' ' + today + '\n\
++ ' ' + nowtime + '\n\
 url: /posts/postslug/\n\
 categories:\n\
   - uncategorized\n\
@@ -27,7 +34,43 @@ tags:\n\
   - untagged\n\
 ---\n\
 ';
+function saveToFile()
+{
+    var textToSave = document.getElementById("text").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/html"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var m = prompt ( "请输入文件名" , "" ) ;
 
+    var fileNameToSaveAs = todaydate + "-" + m + ".md";
+ 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
+}
+ 
+function destroyClickedElement(event)
+{
+    document.body.removeChild(event.target);
+}
+ 
+function loadFileAsText()
+{
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+ 
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent) 
+    {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("inputTextToSave").value = textFromFileLoaded;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
 $(function() {
   // When using more than one `textarea` on your page, change the following line to match the one you’re after
   var $textarea = $('textarea'),
