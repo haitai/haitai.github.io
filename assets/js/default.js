ecb0ee273f2b7185510703a5f0b21873c67255ef
haitai <github@huhaitai.com>
2018-02-23 00:11:49 +0800

    first commit

--- assets/js/default.js
+++ assets/js/default.js
@@ -110,6 +110,74 @@ $(function() {
   }).trigger('keyup');
 });
 
+// 说明：Javascript 切换页面 CSS 样式
+// 整理：http://www.CodeBit.cn
+
+function setActiveStyleSheet(title) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
+  }
+}
+
+function getActiveStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function getPreferredStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1
+      && a.getAttribute("rel").indexOf("alt") == -1
+      && a.getAttribute("title")
+    ) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+window.onload = function(e) {
+  var cookie = readCookie("style");
+  var title = cookie ? cookie : getPreferredStyleSheet();
+  setActiveStyleSheet(title);
+}
+
+window.onunload = function(e) {
+  var title = getActiveStyleSheet();
+  createCookie("style", title, 365);
+}
+
+var cookie = readCookie("style");
+var title = cookie ? cookie : getPreferredStyleSheet();
+setActiveStyleSheet(title);
+
 function toggleInfo() {
   if(infoStatus == false) {
     var height = $(document.body).height() - 150;
@@ -147,6 +215,7 @@ function toggleColor(){
     $('#preview').removeClass( "night" ).addClass( "day" );
     $('pre').removeClass( "night" ).addClass( "day" );
     $('code').removeClass( "night" ).addClass( "day" );
+    setActiveStyleSheet('prettify-day');
   } else {
     $('body').css("background-color", "black");
     $('body').css("color", "white");
@@ -158,6 +227,7 @@ function toggleColor(){
     $('#preview').removeClass( "day" ).addClass( "night" );
     $('pre').removeClass( "day" ).addClass( "night" );
     $('code').removeClass( "day" ).addClass( "night" );
+    setActiveStyleSheet('prettify-night');
   }
   colorStatus = !colorStatus;
 }
